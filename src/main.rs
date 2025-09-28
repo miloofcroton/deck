09cdfa64da99f6faa84af9dcb6f8f47d24b333e7
Jack Toumey <jack@toumey.io>
2025-09-27 18:57:52 -0700

    pausing to check in this code

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#[derive(Debug)]
+struct Deck {
+  cards: Vec<String>,
+}
+
+impl Deck {
+  fn new() -> Deck {
+
+  }
+}
+
+fn main() {
+  let suits = [
+    "Spades",
+    "Clubs",
+    "Hearts",
+    "Diamonds",
+  ];
+  let values = [
+    "Ace",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+  ];
+
+  let mut cards = vec![];
+
+  for suit in suits {
+    for value in values {
+      let card = format!("{} of {}", value, suit);
+      cards.push(card)
+    }
+  }
+
+  // let deck = Deck { cards: Vec::new() };
+  // let deck = Deck { cards: vec![] };
+  // let deck = Deck { cards: cards };
+  let deck = Deck { cards };
+
+  // println!("Here's your deck: {deck?}");
+  println!("Here's your deck: {:#?}", deck);
+}
