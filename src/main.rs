7e025a9edbe1565ca0f5ba35eadee53773c070d0
Jack Toumey <jack@toumey.io>
2025-09-27 19:16:16 -0700

    moving code to inherent implementation

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,50 @@ struct Deck {
 }
 
 impl Deck {
-  fn new() -> Deck {
+  fn new() -> Self {
+    let suits = [
+      "Spades",
+      "Clubs",
+      "Hearts",
+      "Diamonds",
+    ];
+    let values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King",
+    ];
 
+    let mut cards = vec![];
+
+    for suit in suits {
+      for value in values {
+        let card = format!("{} of {}", value, suit);
+        cards.push(card)
+      }
+    }
+
+    // let deck = Deck { cards: Vec::new() };
+    // let deck = Deck { cards: vec![] };
+    // let deck = Deck { cards: cards };
+    let deck = Deck { cards };
+
+    return deck;
   }
 }
 
 fn main() {
-  let suits = [
-    "Spades",
-    "Clubs",
-    "Hearts",
-    "Diamonds",
-  ];
-  let values = [
-    "Ace",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-    "Ten",
-    "Jack",
-    "Queen",
-    "King",
-  ];
 
-  let mut cards = vec![];
-
-  for suit in suits {
-    for value in values {
-      let card = format!("{} of {}", value, suit);
-      cards.push(card)
-    }
-  }
-
-  // let deck = Deck { cards: Vec::new() };
-  // let deck = Deck { cards: vec![] };
-  // let deck = Deck { cards: cards };
-  let deck = Deck { cards };
+  let deck = Deck::new();
 
   // println!("Here's your deck: {deck?}");
   println!("Here's your deck: {:#?}", deck);
